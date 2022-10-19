604b99f5ca395353363dabf2dabe61b6d8a509d1
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-10-19 14:10:39 -0300

    refactor: Login page to Typescript

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {
+  FunctionComponent,
+  InputHTMLAttributes,
+} from "react";
 import { Input } from "./CustomInputStyles";
 
-export const CustomInput = React.forwardRef(
-  (props, ref) => {
+interface CustomInputInterface
+  extends InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+  type?: string;
+  name?: string;
+}
+
+export const CustomInput: FunctionComponent<CustomInputInterface> =
+  React.forwardRef((props, ref) => {
     return <Input {...props} ref={ref as any} />;
-  }
-);
+  });
+CustomInput.displayName = "CustomInput";
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -8,15 +8,21 @@ import { FiLogIn } from "react-icons/fi";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { Link } from "react-router-dom";
 
 // Components
 import { CustonButton } from "../../components/CustomButton/CustonButton";
 import { CustomInput } from "../../components/CustomInput/CustomInput";
 import { CustomLabel } from "../../components/CustomLabels/CustomLabels";
+import { InputErrorMessage } from "../../components/errorMessage/errorMessage";
+
+// Utilities
 import { UserContext } from "../../context/UserContext";
 
-import { InputErrorMessage } from "../../components/errorMessage/errorMessage";
-import { Link } from "react-router-dom";
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 export const Login = () => {
   const { handleForm } = useContext(UserContext);
@@ -24,30 +30,16 @@ export const Login = () => {
   const loginScheme = yup.object().shape({
     email: yup
       .string()
-      .required(
-        <InputErrorMessage>
-          E-mail obrigatório
-        </InputErrorMessage>
-      )
-      .email(
-        <InputErrorMessage>
-          E-mail inválido, tente outro e-mail
-        </InputErrorMessage>
-      ),
-    password: yup
-      .string()
-      .required(
-        <InputErrorMessage>
-          Senha obrigatória
-        </InputErrorMessage>
-      ),
+      .required("E-mail obrigatório")
+      .email("E-mail inválido, tente outro e-mail"),
+    password: yup.string().required("Senha obrigatória"),
   });
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
+  } = useForm<FormValues>({
     resolver: yupResolver(loginScheme),
   });
 
@@ -63,21 +55,23 @@ export const Login = () => {
             E-mail
           </CustomLabel>
           <CustomInput
-            name={"email"}
             placeholder="Digite seu e-mail"
             {...register("email")}
           />
-          {errors.email?.message}
+          <InputErrorMessage>
+            {errors.email?.message}
+          </InputErrorMessage>
           <CustomLabel labelName={"password"}>
             Senha
           </CustomLabel>
           <CustomInput
-            name={"password"}
             placeholder="Digite sua senha"
             type="password"
             {...register("password")}
           />
-          {errors.password?.message}
+          <InputErrorMessage>
+            {errors.password?.message}
+          </InputErrorMessage>
           <h2>Login</h2>
           <CustonButton startIcon={<FiLogIn size={18} />}>
             Entrar
